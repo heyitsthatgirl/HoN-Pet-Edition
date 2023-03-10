2d4fb2bb278fa46e586138de8c3eafaa23875b14
Bronson Wardle <bronson.wardle@gmail.com>
2023-03-09 20:56:32 -0500

    added all server code,a route, schema code, config code, controller index.js code, env code

---
+++ controllers/home-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+// const { Gallery, Painting } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {
+    // const dbGalleryData = await Gallery.findAll({
+    //   include: [
+    //     {
+    //       model: Painting,
+    //       attributes: ['filename', 'description'],
+    //     },
+    //   ],
+    // });
+
+    // const galleries = dbGalleryData.map((gallery) =>
+    //   gallery.get({ plain: true })
+    // );
+
+    res.render('loginpage', {
+      
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
