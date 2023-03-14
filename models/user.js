b344672459f843a2bf1b634d4bbc07ed5d68e844
Bronson Wardle <bronson.wardle@gmail.com>
2023-03-13 20:24:29 -0400

    created a user model.

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+// set PW Var
+class User extends Model {
+  checkPassword() {
+    return compareSync(this.password);
+  }
+}
+
+
+User.init(
+{
+  id:{
+    type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
+    },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      len: [10],
+    },
+  },
+
+  sequelize,
+  timestamps: false,
+  freezeTableName: true,
+  underscored: true,
+  modelName:'user',
+
+
+});
+
+module.exports = User;
