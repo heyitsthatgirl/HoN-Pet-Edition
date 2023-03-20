fd4606e2d403b0e9495ba9455bf76dda865c0bcd
hopemansfield <hopemansfield@gmail.com>
2023-03-20 09:43:05 -0400

    logout route working when logout button is clicked

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -31,7 +31,7 @@ router.post("/login", async (req, res) => {
 	}
 });
 
-router.get("/logout", (req, res) => {
+router.post("/logout", (req, res) => {
 	if (req.session.logged_in) {
 		req.session.destroy(() => {
 			res.status(204).end();
--- public/JS/script.js
+++ public/JS/script.js
@@ -1,32 +1,49 @@
 // script.js
 const Home = document.getElementById("Home");
+const logout = document.getElementById("logout");
 const form = document.getElementById("form");
 
 form.addEventListener("submit", submitForm);
 
 function submitForm(e) {
-	e.preventDefault();
-	const name = document.getElementById("name");
-	const files = document.getElementById("image2");
-	const formData = new FormData();
+  e.preventDefault();
+  const name = document.getElementById("name");
+  const files = document.getElementById("image2");
+  const formData = new FormData();
 
-	formData.append("name", name.value);
-	for (let i = 0; i < files.files.length; i++) {
-		formData.append("image2", files.files[i]);
-	}
-	fetch("/upload", {
-		method: "POST",
-		body: formData,
-		// headers: {
-		//   "Content-Type": "multipart/form-data"
-		// }
-	})
-		.then((res) => console.log(res))
-		.catch((err) => ("Error occured", err));
+  formData.append("name", name.value);
+  for (let i = 0; i < files.files.length; i++) {
+    formData.append("image2", files.files[i]);
+  }
+  fetch("/upload", {
+    method: "POST",
+    body: formData,
+    // headers: {
+    //   "Content-Type": "multipart/form-data"
+    // }
+  })
+    .then((res) => console.log(res))
+    .catch((err) => ("Error occured", err));
+}
+
+
+function goHome() {
+  document.location.replace("/homepage");
 }
 
 Home.addEventListener("click", goHome);
 
-function goHome() {
-	document.location.replace('/homepage');
+async function logoutUser() {
+  const response = await fetch("/api/user/logout", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert("Failed to log out.");
+  }
 }
+
+logout.addEventListener("click", logoutUser);
