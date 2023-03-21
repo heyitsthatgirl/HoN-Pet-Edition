5c4e6b4d7ae43b0166399657799ff9f3348f0121
hopemansfield <hopemansfield@gmail.com>
2023-03-20 20:03:39 -0400

    seeded pics database

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,9 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, pics } = require('../models');
+
 
 const userData = require('./userData.json');
+const picsData = require('./picsData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +12,8 @@ const seedDatabase = async () => {
     individualHooks: true,
     returning: true,
   });
+
+ await pics.bulkCreate(picsData);
 
   process.exit(0);
 };
