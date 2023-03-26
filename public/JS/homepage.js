3e2ac178a88838b3319e3843af4433c58e4d17b8
nah <hef9874@gmail.com>
2023-03-26 16:51:06 -0400

    Added more images to the homepage.js and logo to nav bar

--- public/JS/homepage.js
+++ public/JS/homepage.js
@@ -19,3 +19,14 @@ Not.addEventListener("click", async function () {
   console.log("NOT");
   pictId++;
 });
+
+window.onload = choosePic;
+
+window.onclick = choosePic;
+
+var myPix = new Array("https://res.cloudinary.com/di4jj6chw/image/upload/v1679348306/b75mwkrlsid6r7eocxde.webp", "https://res.cloudinary.com/di4jj6chw/image/upload/v1679346970/svmgqxjkh1iybnp2mtij.jpg", "https://res.cloudinary.com/di4jj6chw/image/upload/v1679336634/rtslrmbwxom4q2frfbfp.jpg", "https://res.cloudinary.com/di4jj6chw/image/upload/v1679763025/m7yfob3kkyew4rr1m5od.jpg", "http://res.cloudinary.com/di4jj6chw/image/upload/v1679862234/xrupxmksicp5ww9jws7p.jpg", "http://res.cloudinary.com/di4jj6chw/image/upload/v1679863443/hwjooc5leyubxpjqrjbf.jpg");
+
+function choosePic() {
+     var randomNum = Math.floor(Math.random() * myPix.length);
+     document.getElementById("homePic").src = myPix[randomNum];
+}
